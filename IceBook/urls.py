b37a7693c342e62ed1cf8a291feef17e5506bdf5
icecream <zanjarwhite@gmail.com>
2019-04-05 00:21:45 +0600

    'done create views'

--- IceBook/urls.py
+++ IceBook/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path('tweets/',include('tweets.urls',namespace="tweets")),
+    path('tweet/',include(('tweets.urls','tweets'),namespace='tweet')),
 ]
